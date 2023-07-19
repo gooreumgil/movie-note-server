3a742c507aee539365ea9257922c48badc6fe9a4
gooreumgil <gooreumgil47@gmail.com>
2023-07-19 20:56:47 +0900

    domain 작업

---
+++ movie-note-core/src/main/java/com/oldteam/movienote/core/domain/movie/MovieReviewLikeHistory.java
@@ -0,0 +1,30 @@
+package com.oldteam.movienote.core.domain.movie;
+
+import com.oldteam.movienote.core.audit.AuditingDomain;
+import com.oldteam.movienote.core.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieReviewLikeHistory extends AuditingDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "movie_review_id")
+    private MovieReview movieReview;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+}
