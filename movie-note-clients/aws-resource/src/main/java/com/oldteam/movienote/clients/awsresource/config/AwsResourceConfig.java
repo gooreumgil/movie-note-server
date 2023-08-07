e973b7901c1b3834362ecda22b1f49177d894b2c
gooreumgil <gooreumgil47@gmail.com>
2023-08-07 21:08:28 +0900

    aws 작업

--- movie-note-clients/aws-resource/src/main/java/com/oldteam/movienote/clients/awsresource/config/AwsResourceConfig.java
+++ movie-note-clients/aws-resource/src/main/java/com/oldteam/movienote/clients/awsresource/config/AwsResourceConfig.java
@@ -1,8 +1,14 @@
 package com.oldteam.movienote.clients.awsresource.config;
 
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.*;
 import com.oldteam.movienote.clients.awsresource.service.AwsS3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.autoconfigure.context.properties.AwsCredentialsProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +16,25 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class AwsResourceConfig {
 
-    private final AmazonS3 amazonS3;
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
 
     @Bean
     public AwsS3Service awsS3Service() {
-        return new AwsS3Service(amazonS3);
+        return new AwsS3Service(amazonS3());
+    }
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSStaticCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(basicAWSCredentials);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withRegion(Regions.AP_NORTHEAST_2)
+                .build();
     }
 
 }
